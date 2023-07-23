082653520d6c846cff968f98ca497d9bc551c6e4
Cong Le <congldqn888@gmail.com>
2023-07-24 00:33:57 +0700

    config mongoose connect

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const express = require("express");
-require("dotenv").config();
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +9,10 @@ const port = process.env.PORT || 8888;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(
+  "mongodb+srv://root:congltph27602@asm-web209.hu4clmr.mongodb.net/?retryWrites=true&w=majority"
+);
 
 app.listen(port, () => {
   console.log(`Server running on the port: ${port}`);
