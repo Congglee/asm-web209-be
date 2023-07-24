b124643007776cacc6bf63eff68c38ecf528eaf8
Cong Le <congldqn888@gmail.com>
2023-07-25 03:21:23 +0700

    hotfix: fix authentication and jwt

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,24 +1,25 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 const generateAccessToken = (user) => {
-    return jwt.sign(
-        {
-            _id: user.id,
-            role: user.role,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: '1d' })
-}
+  return jwt.sign(
+    {
+      _id: user.id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+};
 
 const generateRefreshToken = (user) => {
-    return jwt.sign(
-        {
-            _id: user.id,
-            role: user.role,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "3d" }
-    );
+  return jwt.sign(
+    {
+      _id: user.id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "3d" }
+  );
 };
 
 export { generateAccessToken, generateRefreshToken };
--- routers/user.js
+++ routers/user.js
@@ -1,6 +1,8 @@
-import { register, login, logOut } from "../controllers/user";
+import express from "express";
+import { register, login, logOut } from "../controllers/user.js";
 
 const router = express.Router();
+
 router.post("/users/register", register);
 router.post("/users/login", login);
 router.post("/users/logOut", logOut);
